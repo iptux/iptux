d6aeb4202ea3c2cf83e27af63d69d6a9a9f55836
iptux <iptux7@gmail.com>
2013-01-09 01:54:24 +0800

    alarm.sh: small fix

--- cm/alarm.sh
+++ cm/alarm.sh
@@ -7,11 +7,15 @@
 # Create: 2012-01-08 03:10
 
 
+ALARM_FILE=/media/ext3/cm/Hassium.ogg
+
+
 function AlarmMe () {
+	echo "Press Ctrl+C to stop alarm"
 	# -loop 0: endless loop, need Ctrl+C to stop
 	# -af volume=10.1: 10dB volume
 	# 2>/dev/null 1&>2: output nothing
-	mplayer -loop 0 -af volume=10 /media/ext3/cm/Hassium.ogg 2>/dev/null 1&>2
+	mplayer -loop 0 -af volume=10 ${ALARM_FILE} 2>/dev/null 1>&2
 }
 
 AlarmMe
