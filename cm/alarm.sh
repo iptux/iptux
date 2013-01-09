9487fa3ab20eb5b94456ab534384c621aaad2408
iptux <iptux7@gmail.com>
2013-01-09 18:53:13 +0800

    alarm.sh: fool mistake on redirection

--- cm/alarm.sh
+++ cm/alarm.sh
@@ -12,10 +12,11 @@ ALARM_FILE=/media/ext3/cm/Hassium.ogg
 
 function AlarmMe () {
 	echo "Press Ctrl+C to stop alarm"
+
 	# -loop 0: endless loop, need Ctrl+C to stop
-	# -af volume=10.1: 10dB volume
-	# 2>/dev/null 1&>2: output nothing
-	mplayer -loop 0 -af volume=10 ${ALARM_FILE} 2>/dev/null 1>&2
+	# -af volume=10: 10dB volume
+	# >/dev/null 2&>1: output nothing
+	mplayer -loop 0 -af volume=10 ${ALARM_FILE} >/dev/null 2>&1
 }
 
 AlarmMe
